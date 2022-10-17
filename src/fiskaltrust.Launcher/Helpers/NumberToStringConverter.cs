2356967a0b94c1dc0c8bfd43aefe39a79cc75e87
Paul Volavsek <paul.volavsek@gmail.com>
2022-10-17 12:22:23 +0200

    cleanup

--- src/fiskaltrust.Launcher/Helpers/NumberToStringConverter.cs
+++ src/fiskaltrust.Launcher/Helpers/NumberToStringConverter.cs
@@ -6,10 +6,6 @@ namespace fiskaltrust.Launcher.Helpers
 {
     public class NumberToStringConverter : JsonConverter<string>
     {
-        public override bool CanConvert(Type typeToConvert)
-        {
-            return typeof(string) == typeToConvert;
-        }
         public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             if (reader.TokenType == JsonTokenType.Number)
@@ -30,8 +26,7 @@ namespace fiskaltrust.Launcher.Helpers
                 return reader.GetString();
             }
 
-            using JsonDocument document = JsonDocument.ParseValue(ref reader);
-            return document.RootElement.Clone().ToString();
+            return null;
         }
 
         public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
