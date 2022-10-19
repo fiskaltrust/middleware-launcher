ae695b8164aece85e723aaaa79d43ffbd88a7bda
Paul Volavsek <paul.volavsek@gmail.com>
2022-10-19 14:24:04 +0200

    add logging tests

--- src/fiskaltrust.Launcher/Logging/CollectionSink.cs
+++ src/fiskaltrust.Launcher/Logging/CollectionSink.cs
@@ -1,11 +1,10 @@
 
-using System.Collections.Concurrent;
 using Serilog.Core;
 using Serilog.Events;
 
 namespace fiskaltrust.Launcher.Logging
 {
-    class CollectionSink : ILogEventSink
+    public class CollectionSink : ILogEventSink
     {
         public List<LogEvent> Events { get; } = new List<LogEvent>();
 
