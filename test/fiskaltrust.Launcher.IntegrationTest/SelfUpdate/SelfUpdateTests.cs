ce2ee7455e060dcc4bcc59ab242aa1b3144a08bc
Paul Volavsek <paul.volavsek@gmail.com>
2022-11-18 14:32:20 +0100

    refactor skipping

--- test/fiskaltrust.Launcher.IntegrationTest/SelfUpdate/SelfUpdateTests.cs
+++ test/fiskaltrust.Launcher.IntegrationTest/SelfUpdate/SelfUpdateTests.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
 using fiskaltrust.Launcher.Commands;
@@ -15,15 +16,10 @@ namespace fiskaltrust.Launcher.IntegrationTest.SelfUpdate
 {
     public class SelfUpdateTests
     {
-        [Fact]
+        // Test is not working on linux right now 🥲
+        [FactSkipIf(OsIs: "linux")]
         public async Task Test()
         {
-            // Test is not working on linux right now 🥲
-            if (OperatingSystem.IsLinux())
-            {
-                return;
-            }
-
             LauncherConfiguration launcherConfiguration = TestLauncherConfig.GetTestLauncherConfig(Guid.Parse("c813ffc2-e129-45aa-8b51-9f2342bdfa08"), "BFHGxJScfQz7OJwIfH4QSYpVJj7mDkC4UYZQDiINXW6PED34hdJQ791wlFXKL+q3vPg/vYgaBSeB9oqyolQgtkE=");
             launcherConfiguration.LauncherVersion = new SemanticVersioning.Range("2.*.* || >=2.0.0-preview1");
             File.WriteAllText(Path.Combine(launcherConfiguration.ServiceFolder!, "launcher.configuration.json"), JsonSerializer.Serialize(launcherConfiguration));
