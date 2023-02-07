1ab210675ab30dae890646c4eccc998a8bd559a4
forsthug <85173816+forsthug@users.noreply.github.com>
2023-02-07 08:00:24 +0100

    exclude consple lpg line as it leads to exception in service

--- src/fiskaltrust.Launcher/Program.cs
+++ src/fiskaltrust.Launcher/Program.cs
@@ -27,7 +27,7 @@ args = args.TakeWhile(a => a != "--").ToArray();
 return await new CommandLineBuilder(command)
   .UseHost(host =>
   {
-      Console.OutputEncoding = Encoding.UTF8;
+      //Console.OutputEncoding = Encoding.UTF8;
       host.UseCustomHostLifetime();
 
       host.ConfigureServices(services => services
