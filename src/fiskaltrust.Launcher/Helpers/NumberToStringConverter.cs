e7a780c2aae9e5a77ac87326bad38b4ae336bd92
Paul Volavsek <paul.volavsek@gmail.com>
2023-10-17 17:34:50 +0200

    use default converter

--- src/fiskaltrust.Launcher/Helpers/NumberToStringConverter.cs
+++ src/fiskaltrust.Launcher/Helpers/NumberToStringConverter.cs
@@ -6,6 +6,8 @@ namespace fiskaltrust.Launcher.Helpers
 {
     public class NumberToStringConverter : JsonConverter<string>
     {
+        private readonly static JsonConverter<string> DEFAULT_CONVERTER = (JsonConverter<string>)JsonSerializerOptions.Default.GetConverter(typeof(string));
+
         public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             if (reader.TokenType == JsonTokenType.Number)
@@ -29,9 +31,6 @@ namespace fiskaltrust.Launcher.Helpers
             return null;
         }
 
-        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
-        {
-            writer.WriteStringValue(value.ToString());
-        }
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) => DEFAULT_CONVERTER.Write(writer, value, options);
     }
 }
