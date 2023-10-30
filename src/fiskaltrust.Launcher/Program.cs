50e1a675b0ae86493c2a677b135b59fe1672891e
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-10-30 16:49:22 +0100

    Default to run command when no arguments are provided

--- src/fiskaltrust.Launcher/Program.cs
+++ src/fiskaltrust.Launcher/Program.cs
@@ -7,50 +7,53 @@ using fiskaltrust.Launcher.Extensions;
 using fiskaltrust.Launcher.Helpers;
 
 var command = new RootCommand {
-  new RunCommand(),
-  new HostCommand() { IsHidden = true },
-  new InstallCommand(),
-  new UninstallCommand(),
-  new ConfigCommand(),
-  new DoctorCommand(),
+    new RunCommand(),
+    new HostCommand() { IsHidden = true },
+    new InstallCommand(),
+    new UninstallCommand(),
+    new ConfigCommand(),
+    new DoctorCommand(),
 };
 
 command.Handler = System.CommandLine.Invocation.CommandHandler.Create(() =>
 {
-  Console.Error.WriteLine($"Please specify a command to run this application. Use '--help' for more information.");
+    Console.Error.WriteLine($"Please specify a command to run this application. Use '--help' for more information.");
 });
 
 var subArguments = new SubArguments(args.SkipWhile(a => a != "--").Skip(1));
 args = args.TakeWhile(a => a != "--").ToArray();
 
+if (!args.Any())
+{
+    // If there are no arguments, add arguments for the "run" command
+    args = new[] { "run" };
+}
+
 return await new CommandLineBuilder(command)
-  .UseHost(host =>
-  {
-    host.UseCustomHostLifetime();
+    .UseHost(host =>
+    {
+        host.UseCustomHostLifetime();
 
-    host.ConfigureServices(services => services
-      .Configure<HostOptions>(opts => opts.ShutdownTimeout = TimeSpan.FromSeconds(45))
-      .AddSingleton(_ => subArguments)
-      .AddSingleton(_ => new LauncherProcessId(Environment.ProcessId))
-      .AddSingleton(_ => new LauncherExecutablePath
-      {
-        Path = Environment.ProcessPath ?? throw new Exception("Could not find launcher executable")
-      })
-      .AddSingleton<SelfUpdater>()
-    );
+        host.ConfigureServices(services => services
+            .Configure<HostOptions>(opts => opts.ShutdownTimeout = TimeSpan.FromSeconds(45))
+            .AddSingleton(_ => subArguments)
+            .AddSingleton(_ => new LauncherProcessId(Environment.ProcessId))
+            .AddSingleton(_ => new LauncherExecutablePath
+            {
+                Path = Environment.ProcessPath ?? throw new Exception("Could not find launcher executable")
+            })
+            .AddSingleton<SelfUpdater>()
+        );
 
-    host
-      .UseCommandHandler<HostCommand, HostCommandHandler>()
-      .UseCommandHandler<RunCommand, RunCommandHandler>()
-      .UseCommandHandler<InstallCommand, InstallCommandHandler>()
-      .UseCommandHandler<UninstallCommand, UninstallCommandHandler>()
-      .UseCommandHandler<ConfigGetCommand, ConfigGetCommandHandler>()
-      .UseCommandHandler<ConfigSetCommand, ConfigSetCommandHandler>()
-      .UseCommandHandler<DoctorCommand, DoctorCommandHandler>();
-  })
-  .UseHelp()
-  .UseVersionOption()
-  .Build().InvokeAsync(args);
-
-
-
+        host
+            .UseCommandHandler<HostCommand, HostCommandHandler>()
+            .UseCommandHandler<RunCommand, RunCommandHandler>()
+            .UseCommandHandler<InstallCommand, InstallCommandHandler>()
+            .UseCommandHandler<UninstallCommand, UninstallCommandHandler>()
+            .UseCommandHandler<ConfigGetCommand, ConfigGetCommandHandler>()
+            .UseCommandHandler<ConfigSetCommand, ConfigSetCommandHandler>()
+            .UseCommandHandler<DoctorCommand, DoctorCommandHandler>();
+    })
+    .UseHelp()
+    .UseVersionOption()
+    .Build().InvokeAsync(args);
