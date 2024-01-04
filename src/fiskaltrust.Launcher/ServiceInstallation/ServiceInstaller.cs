c0104841e202ab0844b9c51535635d48cc68ee6c
PawelKarczewski <karczewskipawel.j@gmail.com>
2024-01-04 14:49:03 +0100

    Replaced RunProcess with ProcessHelper

--- src/fiskaltrust.Launcher/ServiceInstallation/ServiceInstaller.cs
+++ src/fiskaltrust.Launcher/ServiceInstallation/ServiceInstaller.cs
@@ -17,42 +17,7 @@ namespace fiskaltrust.Launcher.ServiceInstallation
         public abstract Task<int> InstallService(string commandArgs, string? displayName, bool delayedStart = false);
 
         public abstract Task<int> UninstallService();
-
-        public static async Task<(int exitCode, string output)> RunProcess(string fileName, IEnumerable<string> arguments)
-        {
-            var process = new Process();
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.FileName = fileName;
-            process.StartInfo.CreateNoWindow = false;
-
-            process.StartInfo.Arguments = string.Join(" ", arguments);
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.RedirectStandardOutput = true;
-
-            process.Start();
-
-            await process.WaitForExitAsync();
-
-            var withEnrichedContext = (Action log) =>
-            {
-                var enrichedContext = LogContext.PushProperty("EnrichedContext", $" {Path.GetFileName(fileName)}");
-                log();
-                enrichedContext.Dispose();
-            };
-
-            var stdOut = await process.StandardOutput.ReadToEndAsync();
-            if (!string.IsNullOrEmpty(stdOut))
-            {
-                withEnrichedContext(() => Log.Information(stdOut));
-            }
-
-            var stdErr = await process.StandardError.ReadToEndAsync();
-            if (!string.IsNullOrEmpty(stdErr))
-            {
-                withEnrichedContext(() => Log.Error(stdErr));
-            }
-
-            return (process.ExitCode, stdOut);
-        }
+        
+        // The RunProcess method has been removed, as we now use ProcessHelper.RunProcess instead.
     }
 }
