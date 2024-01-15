debee08320707d0cd6a45c5f97b3ae1cd24fe5de
forsthug <85173816+forsthug@users.noreply.github.com>
2024-01-15 10:21:45 +0100

    ..

--- src/fiskaltrust.Launcher/ServiceInstallation/LinuxSystemD.cs
+++ src/fiskaltrust.Launcher/ServiceInstallation/LinuxSystemD.cs
@@ -101,8 +101,7 @@ namespace fiskaltrust.Launcher.ServiceInstallation
 
         private static async Task<bool> IsSystemdServiceInstalled(string serviceName)
         {
-            Log.Information($"systemctl list-unit-files --type service | grep -F \"{serviceName}.service\"");
-            var (exitCode, output) = await ProcessHelper.RunProcess("systemctl", new[] { $"systemctl list-unit-files --type service | grep -F \"{serviceName}.service\"" });
+            var (exitCode, output) = await ProcessHelper.RunProcess("systemctl", new[] { $"list-unit-files --type service | grep -F \"{serviceName}.service\"" });
             Log.Information($"exitCode: {exitCode}, output: {output}");
             if (exitCode != 0 && !output.Contains("serviceName"))
             {
