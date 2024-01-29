f9fb2c91babb1dbf7407e942195b95af08dddc7f
PawelKarczewski <karczewskipawel.j@gmail.com>
2024-01-29 17:15:00 +0100

    Resolve diff

--- src/fiskaltrust.Launcher/Commands/Common.cs
+++ src/fiskaltrust.Launcher/Commands/Common.cs
@@ -218,12 +218,18 @@ namespace fiskaltrust.Launcher.Commands
                 .Enrich.FromLogContext()
                 .CreateLogger();
 
-        foreach (var logEvent in collectionSink.Events) Log.Write(logEvent);
+            foreach (var logEvent in collectionSink.Events)
+            {
+                Log.Write(logEvent);
+            }
 
-        // If any critical errors occured, we exit with a non-zero exit code.
-        // In many cases we don't want to immediately exit the application,
-        // but we want to log the error and continue and see what else is going on before we exit.
-        if (collectionSink.Events.Where(e => e.Level == LogEventLevel.Fatal).Any()) return 1;
+            // If any critical errors occured, we exit with a non-zero exit code.
+            // In many cases we don't want to immediately exit the application,
+            // but we want to log the error and continue and see what else is going on before we exit.
+            if (collectionSink.Events.Where(e => e.Level == LogEventLevel.Fatal).Any())
+            {
+                return 1;
+            }
 
             Log.Debug("Launcher Configuration File: {LauncherConfigurationFile}", options.LauncherConfigurationFile);
             Log.Debug("Cashbox Configuration File: {CashboxConfigurationFile}", launcherConfiguration.CashboxConfigurationFile);
@@ -231,18 +237,16 @@ namespace fiskaltrust.Launcher.Commands
 
             Log.Debug("Launcher running as {ServiceType}", Enum.GetName(typeof(ServiceTypes), host.Services.GetRequiredService<ServiceType>().Type));
 
-        var dataProtectionProvider = DataProtectionExtensions.Create(launcherConfiguration.AccessToken,
-            useFallback: launcherConfiguration.UseLegacyDataProtection!.Value);
+            var dataProtectionProvider = DataProtectionExtensions.Create(launcherConfiguration.AccessToken, useFallback: launcherConfiguration.UseLegacyDataProtection!.Value);
 
-        try
-        {
-            launcherConfiguration.Decrypt(
-                dataProtectionProvider.CreateProtector(LauncherConfiguration.DATA_PROTECTION_DATA_PURPOSE));
-        }
-        catch (Exception e)
-        {
-            Log.Warning(e, "Error decrypring launcher configuration file.");
-        }
+            try
+            {
+                launcherConfiguration.Decrypt(dataProtectionProvider.CreateProtector(LauncherConfiguration.DATA_PROTECTION_DATA_PURPOSE));
+            }
+            catch (Exception e)
+            {
+                Log.Warning(e, "Error decrypring launcher configuration file.");
+            }
 
             return await handler(options, new CommonProperties(launcherConfiguration, cashboxConfiguration, clientEcdh!, dataProtectionProvider), specificOptions, host.Services.GetRequiredService<S>());
         }
@@ -291,13 +295,11 @@ namespace fiskaltrust.Launcher.Commands
             }
         }
 
-    public static async Task<ECDiffieHellman> LoadCurve(Guid cashboxId, string accessToken, string serviceFolder,
-        bool useOffline = false, bool dryRun = false, bool useFallback = false)
-    {
-        Log.Verbose("Loading Curve.");
-        var dataProtector = DataProtectionExtensions.Create(accessToken, useFallback: useFallback)
-            .CreateProtector(CashBoxConfigurationExt.DATA_PROTECTION_DATA_PURPOSE);
-        var clientEcdhPath = Path.Combine(serviceFolder, $"client-{cashboxId}.ecdh");
+        public static async Task<ECDiffieHellman> LoadCurve(Guid cashboxId, string accessToken, string serviceFolder, bool useOffline = false, bool dryRun = false, bool useFallback = false)
+        {
+            Log.Verbose("Loading Curve.");
+            var dataProtector = DataProtectionExtensions.Create(accessToken, useFallback: useFallback).CreateProtector(CashBoxConfigurationExt.DATA_PROTECTION_DATA_PURPOSE);
+            var clientEcdhPath = Path.Combine(serviceFolder, $"client-{cashboxId}.ecdh");
 
         if (File.Exists(clientEcdhPath))
             try
